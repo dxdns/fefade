aae9fe67acd9b967e8adcc8a2e35fabe99b55879
dxdns <37380674+dxdns@users.noreply.github.com>
2025-07-15 13:19:16 -0300

    docs: react text field

---
+++ packages/docs/src/lib/components/TextFieldPreview.tsx
@@ -0,0 +1,22 @@
+import { TextField } from "@dxdns/feflow-react"
+
+export default function () {
+	const sizes = ["xs", "sm", "md", "lg", "xl"]
+	const variants = ["contained", "text", "outlined"]
+
+	return (
+		<div style={{ maxWidth: "300px", margin: "0 auto" }}>
+			{variants.map((variant, i) => {
+				const size = sizes[i + 1] as any
+				return (
+					<div key={i}>
+						<TextField label={`${variant} (${size})`
+						} variant={variant as any} size={size} />
+						<br />
+						<br />
+					</div>
+				)
+			})}
+		</div>
+	)
+}
