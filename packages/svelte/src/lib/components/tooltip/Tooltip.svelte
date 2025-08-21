c840c5e0ab72d2641bc931107488f3d01a74e6da
dxdns <37380674+dxdns@users.noreply.github.com>
2025-08-21 14:36:42 -0300

    add: svelte tooltip component

---
+++ packages/svelte/src/lib/components/tooltip/Tooltip.svelte
@@ -0,0 +1,31 @@
+<script lang="ts">
+	import { classMapUtil } from "@feflow-ui/core/utils"
+	import type { TooltipType } from "@feflow-ui/core/types"
+	import type { HTMLAttributes } from "svelte/elements"
+	import styles from "@feflow-ui/core/styles/Tooltip.module.css"
+
+	interface Props
+		extends Omit<HTMLAttributes<HTMLDivElement>, "color">,
+			TooltipType {}
+
+	let {
+		class: className = "",
+		label,
+		position = "top",
+		children,
+		...rest
+	}: Props = $props()
+</script>
+
+<div
+	{...rest}
+	data-tooltip={label}
+	class={classMapUtil(
+		className,
+		[className, styles],
+		[position, styles],
+		styles.tooltip
+	)}
+>
+	{@render children?.()}
+</div>
---
+++ packages/svelte/src/routes/(app)/(examples)/tooltip/+page.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+	import { Tooltip } from "@/index.js"
+
+	const positions = ["top", "left", "right", "bottom"] as const
+</script>
+
+<div
+	style="
+	max-width: 300px; 
+	margin: 5rem auto; 
+	display: flex; 
+	flex-direction: column;
+	gap: 1rem;
+	"
+>
+	{#each positions as position (position)}
+		<Tooltip label={position} {position}>
+			<p>{position}</p>
+		</Tooltip>
+	{/each}
+</div>
