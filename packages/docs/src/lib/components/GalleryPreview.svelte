0b2f21e8dec7e8ccaf97faf588e53fdf77c903a6
dxdns <37380674+dxdns@users.noreply.github.com>
2025-09-11 12:00:30 -0300

    docs: gallery

--- packages/docs/src/lib/components/GalleryPreview.svelte
+++ packages/docs/src/lib/components/GalleryPreview.svelte
@@ -1,131 +1,80 @@
 <script lang="ts">
-	import { Gallery, Modal } from "@dxdns-kit/svelte"
-	import { onDestroy, tick } from "svelte"
-	import type { HTMLVideoAttributes } from "svelte/elements"
+	import { Gallery, Text } from "@dxdns-kit/svelte"
 	import { videoUtil } from "@dxdns-kit/core/utils"
-
-	let isOpen = $state(false)
-	let elImg: HTMLImageElement | undefined
 
 	const sizes = [200, 250, 650, 750, 850, 950, 300, 350, 450, 550]
 
 	const { isVideo } = videoUtil()
-
-	async function handleClick(src: string, size: number) {
-		isOpen = true
-
-		await tick()
-
-		if (elImg) {
-			elImg.src = src
-			elImg.width = size
-			elImg.height = size
-		}
-	}
-
-	onDestroy(() => {
-		elImg = undefined
-	})
 </script>
 
-<Modal
-	{isOpen}
-	style="border: none;"
-	handleClose={() => {
-		isOpen = false
-		elImg = undefined
-	}}
->
-	<Modal.Content
-		style="
-		text-align: center;
-		padding: 0;
-		"
-	>
-		<img alt="" bind:this={elImg} />
-	</Modal.Content>
-</Modal>
+<div style="display: flex; flex-direction: column; gap: 1rem;">
+	<div>
+		<h1>columns</h1>
+		<Gallery columns={1}>
+			{#each sizes.slice(0, 6) as size (size)}
+				{@const src = `https://dummyjson.com/image/${size}`}
+				<Gallery.Image
+					lazy
+					dataSrc={src}
+					alt={`Image ${size}px`}
+					width={size}
+					height={size}
+				/>
+			{/each}
+		</Gallery>
+	</div>
 
-<h1>default</h1>
-<Gallery>
-	{#each sizes.slice(0, 4) as size (size)}
-		{@const src = `https://dummyjson.com/image/${size}`}
-		<Gallery.Item
-			lazy
-			dataSrc={src}
-			alt={`Image ${size}px`}
-			width={size}
-			height={size}
-			onclick={() => {
-				handleClick(src, size)
-			}}
-			style="cursor: pointer;"
-		/>
-	{/each}
-</Gallery>
+	<div>
+		<h1>masonry</h1>
+		<Gallery variant="masonry" gap={4}>
+			{#each sizes as size, i (size)}
+				{@const src =
+					i === 0
+						? "https://res.cloudinary.com/dkufrbqih/video/upload/v1754577218/4_-_J5bdBP9_tifpyb.mp4"
+						: `https://dummyjson.com/image/${size}`}
+				{#if isVideo(src)}
+					<Gallery.Video
+						lazy
+						autoplay
+						muted
+						loop
+						dataSrc={src}
+						viewer={{
+							controls: true,
+							style: `
+							object-fit: cover; 
+							background: black; 
+							padding: 2rem; 
+							height: 300px;
+							`
+						}}
+					>
+						<Text as="h4" lines={1}>
+							Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam
+							cupiditate optio quisquam iusto quod vel esse corporis, beatae
+							alias unde quos nulla possimus vitae natus animi sequi ab.
+							Debitis, facilis?
+						</Text>
 
-<br />
-<h1>columns</h1>
-<Gallery columns={1}>
-	{#each sizes.slice(0, 6) as size (size)}
-		{@const src = `https://dummyjson.com/image/${size}`}
-		<Gallery.Item
-			lazy
-			caption={{
-				label: { text: `title ${size}` },
-				description: { text: `description ${size}` }
-			}}
-			dataSrc={src}
-			alt={`Image ${size}px`}
-			width={size}
-			height={size}
-			onclick={() => {
-				handleClick(src, size)
-			}}
-			style="cursor: pointer;"
-		/>
-	{/each}
-</Gallery>
-
-<br />
-<h1>masonry</h1>
-<Gallery variant="masonry" gap="1rem">
-	{#each sizes as size, i (size)}
-		{@const src =
-			i === 0
-				? "https://res.cloudinary.com/dkufrbqih/video/upload/v1754577218/4_-_J5bdBP9_tifpyb.mp4"
-				: `https://dummyjson.com/image/${size}`}
-		<Gallery.Item
-			lazy
-			{...isVideo(src)
-				? ({
-						autoplay: true,
-						muted: true,
-						loop: true,
-						href: src,
-						target: "_blank"
-					} as HTMLVideoAttributes)
-				: {}}
-			caption={{
-				label: {
-					text: `Lorem, ipsum dolor sit amet consectetur adipisicing elit. Optio doloremque magnam sed aliquam assumenda! ${size}`,
-					lines: 1
-				},
-				description: {
-					text: `Praesentium adipisci, illo, atque non optio sit necessitatibus placeat, sunt ut distinctio repellendus ullam repudiandae! Quaerat. ${size}`,
-					lines: 1
-				}
-			}}
-			dataSrc={src}
-			alt={`Image ${size}px`}
-			width={size}
-			height={size}
-			onclick={() => {
-				if (i > 0) {
-					handleClick(src, size)
-				}
-			}}
-			style="cursor: pointer;"
-		/>
-	{/each}
-</Gallery>
+						<Text as="p" lines={2}>
+							Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas
+							omnis totam minus sequi dolorem reprehenderit architecto fugiat!
+							Consequuntur recusandae, sequi ut assumenda pariatur accusamus
+							nihil. Illo eligendi autem quis deserunt!
+						</Text>
+					</Gallery.Video>
+				{:else}
+					<Gallery.Image
+						lazy
+						dataSrc={src}
+						alt={`Image ${size}px`}
+						viewer={{ width: 500 }}
+					>
+						<h4>{`Image ${size}px`}</h4>
+						<p>description</p>
+					</Gallery.Image>
+				{/if}
+			{/each}
+		</Gallery>
+	</div>
+</div>
